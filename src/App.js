491d025cec9d9e65194e8e331e5f82e60dcae761
sbojorge <sara.bojorge@gmail.com>
2023-07-13 23:34:06 +0000

    Connect the application with the backend API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import './api/axiosDefaults'
 
 function App() {
   return (
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://urbandrf-a23e17dc5cc1.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
