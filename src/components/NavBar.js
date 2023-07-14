6e54170063942864374de1b91085ede5403e150e
sbojorge <sara.bojorge@gmail.com>
2023-07-14 06:43:17 +0000

    Add logo in the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar bg="dark" expand="lg">
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="logo" height="50" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
