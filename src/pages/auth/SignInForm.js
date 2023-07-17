b2aab651dad64bbf2c1b9189d9964250c069c948
sbojorge <sara.bojorge@gmail.com>
2023-07-17 15:43:42 +0000

    Add the signin form and style it

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+
+import styles from "../../styles/SignUpInForm.module.css";
+import btnstyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import iconstyles from "../../styles/NavBar.module.css";
+
+import {
+  Col,
+  Container,
+  Row,
+  Form,
+  Button,
+  Alert,
+  Image,
+} from "react-bootstrap";
+
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={styles.Header}>sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="dark" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="dark" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className={styles.iconbutton}>
+              <span>
+                <Image
+                  className={iconstyles.NavIcon}
+                  src={
+                    "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578784/sign-in.232x256_zkzbs5.webp"
+                  }
+                />
+              </span>
+              <Button type="submit" className={btnstyles.button}>
+                Sign In
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+            </div>
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account yet?
+            <span>
+              <Image
+                className={iconstyles.NavIcon}
+                src={
+                  "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578784/sign-up.256x243_wumyuq.webp"
+                }
+              />
+              <Button className={btnstyles.button}>Sign Up</Button>
+            </span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={
+            "https://res.cloudinary.com/dvvr7cpfs/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1689592543/image-from-rawpixel-id-3821079-jpeg_eokfma.jpg"
+          }
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;
