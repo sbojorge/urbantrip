57cfb13ae9c30e55472f8b484c9b037597abcf5a
sbojorge <sara.bojorge@gmail.com>
2023-07-18 21:21:12 +0000

    Tidy up the notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/PageNotFound.module.css";
+import { Container, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <Container className="text-center pt-4">
+      <Image
+        src="https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689713680/page-not-found.218x256_nqko1h.webp"
+        className={styles.notfound}
+      />
 
-export default NotFound
+      <p className="pt-1">Sorry, page not found!</p>
+      <Link to="/">Click here to go back Home</Link>
+    </Container>
+  );
+};
+
+export default NotFound;
