b3130bfce2e14e5515b831987c8ecd2f13e08bee
sbojorge <sara.bojorge@gmail.com>
2023-07-18 09:30:44 +0000

    Wire up links in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container, Image } from "react-bootstrap";
 // import logo from "../assets/logo.webp";
 import styles from "../styles/NavBar.module.css";
 import { CurrentUserContext } from "../App";
-import { useLocation } from "react-router-dom";
+import { useLocation, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const currentUser = useContext(CurrentUserContext);
@@ -12,16 +12,23 @@ const NavBar = () => {
 
   return (
     <>
-      {location.pathname !== "/signup" && location.pathname !== "/signin" && ( 
+      {location.pathname !== "/signup" && location.pathname !== "/signin" && (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
           <Container>
-            <Navbar.Brand className={styles.logo}>
-              {/* <img src={logo} alt="UrbanTrip_logo" /> */}UrbanTrip
-            </Navbar.Brand>
+            <NavLink to="/">
+              <Navbar.Brand className={styles.logo}>
+                {/* <img src={logo} alt="UrbanTrip_logo" /> */}UrbanTrip
+              </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link className={styles.link}>
+                <NavLink
+                  exact
+                  className={styles.link}
+                  activeClassName={styles.Active}
+                  to="/"
+                >
                   <Image
                     src={
                       "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578981/home-button-icon_bogip3.webp"
@@ -29,8 +36,8 @@ const NavBar = () => {
                     className={styles.NavIcon}
                   />
                   Home
-                </Nav.Link>
-                <Nav.Link className={styles.link}>
+                </NavLink>
+                <NavLink className={styles.link} to="#">
                   <Image
                     src={
                       "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578980/plus-circle.256x256_kpllx5.webp"
@@ -38,8 +45,8 @@ const NavBar = () => {
                     className={styles.NavIcon}
                   />
                   Create
-                </Nav.Link>
-                <Nav.Link className={styles.link}>
+                </NavLink>
+                <NavLink className={styles.Navlink} to="#">
                   <Image
                     src={
                       "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578980/search.256x250_vfghof.webp"
@@ -47,8 +54,8 @@ const NavBar = () => {
                     className={styles.NavIcon}
                   />
                   Search
-                </Nav.Link>
-                <Nav.Link className={styles.link}>
+                </NavLink>
+                <NavLink className={styles.link} to="#">
                   <Image
                     src={
                       "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689578981/logout-icon_gvsj1w.webp"
@@ -56,7 +63,7 @@ const NavBar = () => {
                     className={styles.NavIcon}
                   />
                   Sign out
-                </Nav.Link>
+                </NavLink>
               </Nav>
               <Navbar.Text className={styles.link}>
                 Signed in as: {currentUser ? loggedInUser : ""}
@@ -64,7 +71,8 @@ const NavBar = () => {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-      )};
+      )}
+      ;
     </>
   );
 };
