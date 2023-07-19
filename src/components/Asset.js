947115de3afef7b42ee6891cdbd1847debef74bf
sbojorge <sara.bojorge@gmail.com>
2023-07-19 11:35:54 +0000

    Create the Asset component and its CSS

---
+++ src/components/Asset.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/Asset.module.css";
+import { Spinner } from "react-bootstrap";
+
+const Asset = ({ spinner, src, message }) => {
+  return (
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && (
+        <Spinner
+          animation="grow"
+          variant="info" // change the color in the css
+          role="status"
+          aria-hidden="true"
+        />
+      )}
+      {src && <img src={src} alt="" />}
+      {message && <p className="mt-4">{message}</p>}
+    </div>
+  );
+};
+
+export default Asset;
