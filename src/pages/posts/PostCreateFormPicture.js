ce3c0da6806e4b9db8e543180b24ecec5117cf82
sbojorge <sara.bojorge@gmail.com>
2023-07-19 20:43:11 +0000

    Create the PostCreatFormPicture for letting users to upload images

--- src/pages/posts/PostCreateFormPicture.js
+++ src/pages/posts/PostCreateFormPicture.js
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
 
 const PostCreateFormPicture = () => {
-  return (
-    <div>PostCreateFormPicture</div>
-  )
-}
+  // const [errors, setErrors] = useState({});
 
-export default PostCreateFormPicture
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+  const { title, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          placeholder="This field is optional"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="Description"
+          placeholder="This field is optional"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Button className={`${btnStyles.button} m-3`} onClick={() => {}}>
+        cancel
+      </Button>
+      <Button className={btnStyles.button} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.button} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src="https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689795791/image-upload.96x96_nyevbq.webp"
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default PostCreateFormPicture;
