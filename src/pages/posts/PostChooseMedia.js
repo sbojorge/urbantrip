4768c8087466c354b641b4c5babe52615fb91e66
sbojorge <sara.bojorge@gmail.com>
2023-07-19 17:06:11 +0000

    Add content to the PostChooseMedia component and styling

--- src/pages/posts/PostChooseMedia.js
+++ src/pages/posts/PostChooseMedia.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import styles from "../../styles/ChooseMedia.module.css";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 const PostChooseMedia = () => {
   return (
-    <div>PostChooseMedia</div>
-  )
-}
+    <Row>
+      <Col md={6} className="mt-4">
+        <Container className={styles.Choice}>
+          <div className={styles.first}>
+            <Image
+              className={styles.ImageIcon}
+              src={
+                "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689767375/image-upload.256x256_if4o2m.webp"
+              }
+            />
+            <Link className={styles.Message} to="/posts/create/picture/">
+              Create a post using a picture
+            </Link>
+          </div>
+          <div className={styles.second}>
+            <Image
+              className={styles.ImageIcon}
+              src={
+                "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1689767375/film.256x256_lclnzm.webp"
+              }
+            />
+            <Link className={styles.Message} to="/posts/create/video/">
+              Create a post using a video
+            </Link>
+          </div>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
 
-export default PostChooseMedia
+export default PostChooseMedia;
