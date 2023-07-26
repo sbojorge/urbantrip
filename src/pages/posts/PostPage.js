eca0bf2014efea9ac9772ef9aa71f5de1a0043b2
sbojorge <sara.bojorge@gmail.com>
2023-07-26 15:36:27 +0000

    Add the infinite scroll functionality in the PostPage for displaying the comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,9 @@ import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 const PostPage = () => {
   const { id } = useParams();
@@ -50,14 +53,20 @@ const PostPage = () => {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setPost={setPost}
-                setComments={setComments}
-              />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>Would like to be the first one to comment?</span>
           ) : (
