03e2df0ae3060d81705e9dd599bfc99444707cfd
sbojorge <sara.bojorge@gmail.com>
2023-07-29 06:00:56 +0000

    Run tests for NavBar component

---
+++ src/components/__tests__/NavBar.test.js
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import { BrowserRouter as Router } from "react-router-dom"
+import NavBar from  "../NavBar"
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
+
+test("renders NavBar", () => {
+    render(
+        <Router>
+            <NavBar />
+        </Router>
+    );
+
+    // screen.debug();
+    const signInLink = screen.getByRole("link", {name: 'Sign in'})
+    expect(signInLink).toBeInTheDocument()
+});
+
+
+test("renders Sign in and Sign up buttons again on log out", async () => {
+    render(
+        <Router>
+          <CurrentUserProvider>
+            <NavBar />
+          </CurrentUserProvider>
+        </Router>
+    );
+
+    const signOutLink = await screen.findByRole("link", { name: 'Sign out' })
+    fireEvent.click(signOutLink);
+
+    const signInLink = await screen.findByRole("link", { name: 'Sign in' });
+    const signUpLink = await screen.findByRole("link", { name: 'Sign up' });
+
+    expect(signInLink).toBeInTheDocument();
+    expect(signUpLink).toBeInTheDocument();
+    
+});
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,18 +6,18 @@ export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
     return res(
       ctx.json({
-        pk: 5,
-        username: "Chat0510",
+        pk: 9,
+        username: "Gati",
         email: "",
         first_name: "",
         last_name: "",
-        profile_id: 4,
+        profile_id: 8,
         profile_image:
-          "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1/media/../default-profile.256x256_lpdl87",
+          "https://res.cloudinary.com/dvvr7cpfs/image/upload/v1/media/../default-user-circle.256x256_qkk4wi",
       })
     );
   }),
-  rest.post(`${baseURL}dj-rest-auth/logout/`, (req,res,ctx) => {
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
-}),
+  }),
 ];
