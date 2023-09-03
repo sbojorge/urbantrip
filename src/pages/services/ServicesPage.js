fb34674f6353223645de0c96467da9402026c5b6
sbojorge <sara.bojorge@gmail.com>
2023-09-03 17:52:14 +0000

    Add the logic in ServicesPage.js to retrieve the list of services

--- src/pages/services/ServicesPage.js
+++ src/pages/services/ServicesPage.js
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Service from "./Service"
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/ServicesPage.module.css";
 
-const ServicesPage = () => {
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import NoResults from "../../assets/NoResults.png"
+
+const ServicesPage = ({ message }) => {
+  const [services, setServices] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const currentUser = useCurrentUser();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const { data } = await axiosReq.get(`/services/?search=${query}`);
+        setServices(data);
+        setHasLoaded(true);
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchServices();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [query, currentUser]);
+
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {services.results.length ? (
+              services.results.map((service) => (
+                <Service key={service.id} {...service} setServices={setServices} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
