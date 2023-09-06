87224d6f33cee37ae61d164ad16a5f4f58d84c86
sbojorge <sara.bojorge@gmail.com>
2023-09-06 16:24:24 +0000

    Update the ServiceCreateEditForm.module.css file

--- src/pages/services/ServiceDetail.js
+++ src/pages/services/ServiceDetail.js
@@ -6,15 +6,20 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Image from "react-bootstrap/Image";
+import Media from "react-bootstrap/Media";
 
 import styles from "../../styles/ServiceCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 
-import { useHistory, useParams } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
+import { Link, useHistory, useParams } from "react-router-dom";
+import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import { Rating } from "react-simple-star-rating";
+import Avatar from "../../components/Avatar";
+import { ControlsDropdown } from "../../components/ControlsDropdown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const ServiceDetail = (props) => {
+  const { owner, servicePage } = props;
   // const [errors, setErrors] = useState({});
 
   const [serviceData, setServiceData] = useState({
@@ -45,6 +50,10 @@ const ServiceDetail = (props) => {
     average_rating,
   } = serviceData;
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const profile_id = currentUser?.profile_id;
+  const is_owner = currentUser?.username === owner;
   const history = useHistory();
   const { id } = useParams();
 
@@ -83,6 +92,19 @@ const ServiceDetail = (props) => {
 
     handleMount();
   }, [history, id]);
+
+  const handleEdit = () => {
+    history.push(`/services/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/services/${id}/`);
+      window.location.href = "/";
+    } catch (err) {
+      // console.log(err);
+    }
+  };
 
   // const handleChange = (event) => {
   //   setServiceData({
@@ -123,7 +145,27 @@ const ServiceDetail = (props) => {
         <Container
           className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
         >
-          <Card>
+          <Card className={styles.Service}>
+            <Card.Body>
+              <Media className="align-items-center justify-content-between">
+                <Link
+                  className={styles.serviceLinks}
+                  to={`/profiles/${profile_id}`}
+                >
+                  <Avatar src={profile_image} height={55} />
+                  {owner}
+                </Link>
+                <div className="d-flex align-items-center">
+                  <span>placeholder</span>
+                  {is_owner && servicePage && (
+                    <ControlsDropdown
+                      handleEdit={handleEdit}
+                      handleDelete={handleDelete}
+                    />
+                  )}
+                </div>
+              </Media>
+            </Card.Body>
             <Image className={appStyles.Image} src={image} rounded />
             <Card.Body>
               <Card.Title className="text-center">{name}</Card.Title>
@@ -158,7 +200,6 @@ const ServiceDetail = (props) => {
                 </ListGroup.Item>
               </ListGroup>
             </Card.Body>
-            
           </Card>
         </Container>
       </Col>
